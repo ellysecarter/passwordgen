724a926eb58703ecf5212ac529ec511b517d5252
Ellyse <85855848+ellysecarter@users.noreply.github.com>
2021-08-20 10:44:29 -0700

    now displays password on screen

--- Assets/index2.js
+++ Assets/index2.js
@@ -1,11 +1,9 @@
 var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("#password");
 var delayInMilliseconds = 1000;
-// Add event listener to generate button
 
-generateBtn.addEventListener("click", writePassword());
 
 function writePassword() {
+  var passwordText = document.querySelector("#password");
   setTimeout (function() {
   var passwordLength = window.prompt(
     "How many characters would you like to have? (Choose between 8 and 128)"
@@ -60,11 +58,15 @@ function writePassword() {
         );
       }
       console.log(password)
+      passwordText.textContent = password;
       return password;
-  }
-  generatePassword ()
+  } 
+  generatePassword ();
 } else {
   window.alert("Password must be between 8 and 128 characters.");
   }
 }, 500);
 }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword());
