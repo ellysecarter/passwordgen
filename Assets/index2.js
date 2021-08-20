783f4bd5cde2f462bc2c60c0b341fce3c6dcc2fc
Ellyse <85855848+ellysecarter@users.noreply.github.com>
2021-08-19 23:25:27 -0700

    debugged js

--- Assets/index2.js
+++ Assets/index2.js
@@ -1,19 +1,22 @@
-// Assignment Code
+// / Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
+
 function writePassword() {
+
   var passwordLength = window.prompt(
     "How many characters would you like to have? (Choose between 8 and 128)"
   );
-
   if (passwordLength >= 8 && passwordLength <= 128) {
     function generatePassword() {
-      var passwordUpper = window.confirm("Would you like uppercase letters?");
       var passwordLower = window.confirm("Would you like lowercase letters?");
+      var passwordUpper = window.confirm("Would you like uppercase letters?");
       var passwordNumeric = window.confirm("Would you like to add numbers?");
       var passwordSpecial = window.confirm(
         "Would you like special characters?"
       );
+
 
       var emptyString = "";
       var stringUpper = "ABCDEFGHIJKLMNOPQRSTUVQYXZ";
@@ -33,6 +36,8 @@ function writePassword() {
       if (passwordSpecial === true) {
         emptyString += stringSpecial;
       }
+
+
       console.log (emptyString)
       var password = "";
       for (var i = 0; i < passwordLength; i++) {
@@ -44,7 +49,6 @@ function writePassword() {
       console.log(password);
       return password;
     }
-
     generatePassword()
   } else {
     window.alert("Password must be between 8 and 128 characters.");
--- index3.js
+++ index3.js
@@ -1,19 +1,11 @@
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
-
-// }
-
-var emptyArr = ""
-var lowerArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','y','z']
-var upperArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-var numericArr = ['1','2','3','4','5','6','7','8','9','0']
-var specialArr = ['@','$','&','!','#','%','^','*','+','=','?','<','>']
+    var emptyString = "";
+      var stringUpper = "ABCDEFGHIJKLMNOPQRSTUVQYXZ";
+      var stringLower = "abcdefghijklmnopqrstuvwxyz";
+      var stringNumeric = "0987654321";
+      var stringSpecial = '!@#$%^&*()_+={}[]|?/><:;"';
 
 function writePassword() { 
   var passwordLength = window.prompt('How many characters would you like to have? (Choose between 8 and 128)');
@@ -30,40 +22,28 @@ function writePassword() {
   }
 
 
-var selectedCharacters = []
-
 // choose options between lowercase, uppercase, numeric, and/or special characters
   var passwordLower = window.confirm('Would you like lowercase letters?');
   if (passwordLower === true) {
     selectedCharacters.push(lowerArr)
-  }
-  else {
-    window.alert ("You have denied lowercase letters.")
   }
 
   var passwordUpper = window.confirm('Would you like uppercase letters?');
   if (passwordUpper === true) {
     selectedCharacters.push(upperArr)
   }
-  else {
-    window.alert ("You have denied uppercase letters.")
-  }
 
   var passwordNumeric = window.confirm('Would you like to add numbers?');
   if (passwordNumeric === true) {
     selectedCharacters.push(numericArr)
   }
-  else {
-    window.alert ("You have denied numeric characters.")
-  }
+
 
   var passwordSpecial = window.confirm('Would you like special characters?');
   if (passwordSpecial === true) {
     selectedCharacters.push(specialArr)
   }
-  else {
-    window.alert ("You have denied special characters.")
-  }
+
 
   console.log (selectedCharacters)
 
@@ -81,6 +61,4 @@ return "random password"
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
 
